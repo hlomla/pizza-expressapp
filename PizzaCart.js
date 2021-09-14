352a579db1711876bfe8875d43755735b5a51b48
hlomla <hlomla.tapuko@gmail.com>
2021-09-14 16:20:35 +0200

    still not adding pizzas

--- PizzaCart.js
+++ PizzaCart.js
@@ -1,26 +1,86 @@
-module.exports = function pizzaCart(pizzas) {
+module.exports = function pizzaCart() {
 
-    var pizzaTotals = pizzas || [];
+    let pizzaTotals = [];
+    var smallPizzaCost = 0; 
+    var medPizzaCost = 0;
+    var largePizzaCost = 0;
     
-    function addPizza(){
-        //var orderPizza = //
-
+    function addPizza(cost){
+        smallPizzaCost = Number(cost.smallPizzaCost);
+        medPizzaCost = Number(cost.medPizzaCost);
+        largePizzaCost = Number(cost.largePizzaCost)
+    }
+    function getPizza(){
+        return{
+            smallPizzaCost,
+            medPizzaCost,
+            largePizzaCost
+        }
     }
 
-    function pizzaSize(){
-        if(pizzaSize === "small") {
-            return pizzaSize++
+    function allPizzas(){
+        return pizzaTotals.length
+    }
+
+	function removePizza(){
+        smallPizzaCost = 0; 
+        medPizzaCost = 0;
+        largePizzaCost = 0;
+    }
+
+    function pizzaSize(pizza){
+        let pizzaCost = 0;
+        if(pizza === "small") {
+            pizzaCost++;
         }
-        if( pizzaSize === "medium") {
-            return pizzaSize++
+        else if( pizza === "medium") {
+            pizzaCost++;
         }
-        if(pizzaSize === "large") {
-            return pizzaSize++
+        else if(pizza === "large") {
+            pizzaCost++;
+        }
+        if(pizza !== undefined){
+            pizzaTotals.push({
+                type: pizza,
+                pizzaCost
+
+            })
         }
 
+    }
+    function pizzaTot(type){
+        let total = 0;
+        for(let i = 0; i < pizzaTotals.length; i++){
+            const ipiztsa = pizzaTotals[i];
+            if(ipiztsa.type === type) {
+                total += ipiztsa.pizzaCost;
+            }
+        }
+        return total;
+    }
+    function totalPizzaCost(){
+        return pizzaTot('small') + pizzaTot('medium') + pizzaTot('large')
+    }
+    function totals(){
+        let smallTotal = pizzaTot('small').toFixed(2)
+        console.log(smallTotal)
+        let mediumTotal = pizzaTot('medium').toFixed(2)
+        let largeTotal = pizzaTot('large').toFixed(2)
+        return{
+            smallTotal,
+            mediumTotal,
+            largeTotal,
+            totalPizzaCost : totalPizzaCost().toFixed(2)
+        }
     }
     return {
         addPizza,
-        pizzaSize
+        getPizza,
+        pizzaSize,
+        allPizzas,
+        removePizza,
+        pizzaTot,
+        totals,
+        totalPizzaCost
     }
 }
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const connectFlash = require('connect-flash');
 const session = require('express-session');
@@ -10,39 +10,39 @@ const Pool = pg.Pool;
 
 let useSSL = false;
 let local = process.env.LOCAL || false;
-if (process.env.DATABASE_URL && !local){
-    useSSL = true;
-} 
+if (process.env.DATABASE_URL && !local) {
+	useSSL = true;
+}
 
 const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5000/my_pizzacart';
 
 const pool = new Pool({
-    connectionString,
-    ssl : useSSL
-  });
-   
+	connectionString,
+	ssl: useSSL
+});
+
 const pizzacart = pizzaCart()
 
 const app = express();
 
 // add more middleware to allow for templating support
 app.set('view engine', 'handlebars');
-app.engine('handlebars', exphbs({ 
-	partialsDir: "./views/partials", 
-	viewPath: './views', 
-	layoutsDir: './views/layouts' 
-	}));
+app.engine('handlebars', exphbs({
+	partialsDir: "./views/partials",
+	viewPath: './views',
+	layoutsDir: './views/layouts'
+}));
 
 // enable the req.body object - to allow us to use HTML forms
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));	
+app.use(express.urlencoded());
 
 
 app.use(session({
-    secret : "<add a secret string here>",
-    resave: false,
-    saveUninitialized: true
-  }));
+	secret: "<add a secret string here>",
+	resave: false,
+	saveUninitialized: true
+}));
 
 
 
@@ -56,19 +56,60 @@ app.use(express.static('public'));
 
 let counter = 0;
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
+
+
 	res.render('index', {
-		counter
+		counter,
+		cost: pizzacart.pizzaSize(),
+		totals: pizzacart.totals()
 	});
 });
 
-app.post('/count', function(req, res) {
+app.post('/count', function (req, res) {
+	// console.log(req.body.code);
 	counter++;
+	pizzacart.pizzaSize({
+		smallTotal: req.body.smallTotal,
+		mediumTotal: req.body.mediumTotal,
+		largeTotal: req.body.largeTotal
+	})
+
+	// console.log(pizzacart.getPizza)
 	res.redirect('/')
 });
 
-const PORT =  process.env.PORT || 3017;
+app.post('/small', (req, res) => {
+	console.log(req.body.smallTotal);
+	pizzacart.pizzaSize(req.body.smallTotal);
+	pizzacart.totals()
+	res.render('index', {
+		cost: pizzacart.pizzaSize(),
+		totals: pizzacart.totals()
+	})
+})
+app.post('/medium', (req, res) => {
+	console.log(req.body.theMedium);
+	pizzacart.pizzaSize(req.body.mediumTotal);
+	pizzacart.totals()
+	res.render('index',{
+		cost: pizzacart.pizzaSize(),
+		totals: pizzacart.totals()
+	}
+	)
+})
+app.post('/large', (req, res) => {
+	console.log(req.body.largeTotal);
+	pizzacart.pizzaSize(req.body.largeTotal);
+	pizzacart.totals()
+	res.render('index',{
+		cost: pizzacart.pizzaSize(),
+		totals: pizzacart.totals()
+	})
+})
+
+const PORT = process.env.PORT || 3017;
 // start  the server and start listening for HTTP request on the PORT number specified...
-app.listen(PORT, function() {
+app.listen(PORT, function () {
 	console.log(`App started on port ${PORT}`)
 });
